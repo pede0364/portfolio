662ce15ae960fe457b8e41965f843052f4949f39
Aimee Pederson <aimee.pederson@gmail.com>
2024-06-07 09:20:56 -0500

    resume-pdf-hell

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -8,6 +8,7 @@ import {
   SparklesIcon,
 } from '@heroicons/react/24/outline';
 
+import resume from '../assets/resume.pdf';
 import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
@@ -44,8 +45,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Aimee Pederson',
+  description: 'Aimee Pederson | Frontend Developer, Conversion Rate Optimization Expert, QA Engineer',
 };
 
 /**
@@ -69,24 +70,23 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Aimee Pederson`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a Minnesota-based <strong className="text-stone-100">Frontend Developer</strong>, who specializes in helping
+        small businesses build and optimize their mobile-first websites.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my free time time, I like to <strong className="text-stone-100">read novels and non-fiction</strong>, work on
+        our <strong className="text-stone-100">garden</strong>, or visit new parks around{' '}
+        <strong className="text-stone-100">Minneapolis and St. Paul</strong>.
       </p>
     </>
   ),
   actions: [
     {
-      href: '/assets/resume.pdf',
+      href: resume,
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
@@ -108,12 +108,12 @@ export const aboutData: About = {
   to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
   in.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Minneapolis, MN', Icon: MapIcon},
+    {label: 'Age', text: '40', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'American', Icon: FlagIcon},
+    {label: 'Interests', text: 'Reading, Gardening, Nature', Icon: SparklesIcon},
+    {label: 'Study', text: 'University of Minnesota, Twin Cities', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Self Employed', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -129,12 +129,12 @@ export const skills: SkillGroup[] = [
         level: 10,
       },
       {
-        name: 'French',
+        name: 'German',
         level: 4,
       },
       {
-        name: 'Spanish',
-        level: 3,
+        name: 'ASL',
+        level: 4,
       },
     ],
   },
@@ -142,16 +142,20 @@ export const skills: SkillGroup[] = [
     name: 'Frontend development',
     skills: [
       {
-        name: 'React',
+        name: 'HTML / CSS',
         level: 9,
       },
       {
-        name: 'Typescript',
+        name: 'JavaScript',
         level: 7,
       },
       {
-        name: 'GraphQL',
-        level: 6,
+        name: 'React',
+        level: 5,
+      },
+      {
+        name: 'SQL',
+        level: 3,
       },
     ],
   },
@@ -161,14 +165,6 @@ export const skills: SkillGroup[] = [
       {
         name: 'Node.js',
         level: 8,
-      },
-      {
-        name: 'Rust',
-        level: 5,
-      },
-      {
-        name: 'Golang',
-        level: 4,
       },
     ],
   },
